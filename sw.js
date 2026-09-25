c7d7e8758e67d58e9a05e777a70d20d0ddb4c898
sheng332 <160328072@qq.com>
2026-09-25 19:42:03 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// 极简 Service Worker，为了满足浏览器安装 PWA 的要求
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
